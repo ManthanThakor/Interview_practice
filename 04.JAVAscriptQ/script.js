2091fb9363d03ec16f716bd1d987934097bbceb6
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-30 14:34:23 +0530

    Interview pra 6

--- 04.JAVAscriptQ/script.js
+++ 04.JAVAscriptQ/script.js
@@ -1,3 +1,11 @@
 function showMessage() {
   alert("Hello, World!");
 }
+
+function changeText() {
+  document.querySelector("h1").textContent = "Hello, World!";
+}
+function changeBackgroundColor() {
+  var element = document.getElementById("container");
+  element.style.backgroundColor = "red";
+}
