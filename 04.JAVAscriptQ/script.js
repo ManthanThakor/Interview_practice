dacf91e9e7f2b52209af4874f6601a29af0db510
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-30 21:26:13 +0530

    Interview pra 8

--- 04.JAVAscriptQ/script.js
+++ 04.JAVAscriptQ/script.js
@@ -33,16 +33,89 @@ function changeBackgroundColor() {
 
 // greet(); // ?
 
-// Function Declaration
-function greet(name) {
-  return `Hello, ${name}!`;
+// // Function Declaration
+// function greet(name) {
+//   return `Hello, ${name}!`;
+// }
+
+// // Function Expression
+// const add = function (a, b) {
+//   return a + b;
+// };
+
+// console.log(add(5, 10)); // ?
+
+// console.log(greet("John Doe")); // ?
+// function showArguments() {
+//   console.log(arguments);
+// }
+
+// showArguments("a", "b", "c");
+// setTimeout(() => {
+//   console.log("Executed once after 1000ms");
+// }, 1000);
+
+// setTimeout(() => {
+//   console.log("Executed twice after 2000ms");
+// }, 2000);
+
+// const interv = setInterval(() => {
+//   console.log("Interval executed");
+// }, 1000);
+
+// clearInterval(interv);
+// const arr = [1, 2, 3, 4, 5];
+// arr.reverse().forEach((value) => {
+//   console.log(value); // Output: 5, 4, 3, 2, 1
+// });
+
+//! promise
+// function fetchData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const success = true;
+//       if (success) {
+//         resolve("Data fetch successfully");
+//       } else {
+//         reject("Data fetch failed");
+//       }
+//     }, 1000);
+//   });
+// }
+
+// fetchData()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+//! async await
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true; // Simulate success or failure
+      if (success) {
+        resolve("Data fetched successfully");
+      } else {
+        reject("Error fetching data");
+      }
+    }, 1000);
+  });
 }
 
-// Function Expression
-const add = function (a, b) {
-  return a + b;
+const fetchAndProcess = async () => {
+  try {
+    console.log("Fetching Data....");
+    const data = await fetchData();
+    console.log(data);
+    // Process data here
+    console.log("Data processed successfully");
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
 };
 
-console.log(add(5, 10)); // ?
-
-console.log(greet("John Doe")); // ?
+fetchAndProcess();
