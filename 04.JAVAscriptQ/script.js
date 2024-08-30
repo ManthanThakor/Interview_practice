24c6c4e532b623982f9b0c3cebc0d8112d65d22f
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-30 17:15:34 +0530

    Interview pra 7

--- 04.JAVAscriptQ/script.js
+++ 04.JAVAscriptQ/script.js
@@ -9,3 +9,40 @@ function changeBackgroundColor() {
   var element = document.getElementById("container");
   element.style.backgroundColor = "red";
 }
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1000); // ?
+// }
+
+// let a = 5;
+// {
+//   let a = 15;
+//   console.log(a); // ?
+// }
+
+// const car = new Object();
+// car.make = "Toyota";
+// car.model = "Camry";
+// car.year = 2020;
+
+// console.log(car); // ?
+
+// function greet() {
+//   console.log("Hello, World!");
+// }
+
+// greet(); // ?
+
+// Function Declaration
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Function Expression
+const add = function (a, b) {
+  return a + b;
+};
+
+console.log(add(5, 10)); // ?
+
+console.log(greet("John Doe")); // ?
